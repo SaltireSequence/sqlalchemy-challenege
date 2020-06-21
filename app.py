826a687f2ba990be6ed67d7672a54fa68ce6de6a
William F <60886603+wtcf90@users.noreply.github.com>
2020-06-20 22:35:49 -0700

    Boilerplate code for app.py
    Initialising app.py

--- app.py
+++ app.py
@@ -1,1 +1,29 @@
+#################################################
+# IMPORTING DEPENDENCIES
+#################################################
+import numpy as np
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+#################################################
+# DATABASE SETUP
+#################################################
+# Creating engine
+engine = create_engine("sqlite:///titanic.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+
+# reflecting the tables
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
